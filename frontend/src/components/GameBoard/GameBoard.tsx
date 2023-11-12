f2d5c9c8752dca538687ab59e8f1a2e500fd65f3
jmccallister93 <jaredmccallister93@gmail.com>
2023-11-11 16:10:12 -0800

    final commit for the day.

--- frontend/src/components/GameBoard/GameBoard.tsx
+++ frontend/src/components/GameBoard/GameBoard.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import './styles.module.scss';
 
 const GameBoard = () => {
-  // Placeholder for rendering logic
+  // Example logic to render cells on the board
   const renderCells = () => {
-    // Logic to render cells or spaces on the board
+    // Create an array of dummy cell elements
+    const cells = [];
+    for (let i = 0; i < 10; i++) {
+      cells.push(<div key={i} className="cell">Cell {i}</div>);
+    }
+    return cells;
   };
 
   return (
     <div className="game-board">
-      {/* {renderCells()} */}
+      {renderCells()}
     </div>
   );
 };
