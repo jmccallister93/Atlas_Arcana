d892c87d562a1f229879a398535b48f558b21a1e
jmccallister93 <jaredmccallister93@gmail.com>
2023-11-28 11:13:04 -0800

    Added wolrd event cards.

---
+++ backend/gameCards/worldEventCards.js
@@ -0,0 +1,66 @@
+const worldEventCards = [
+  {
+    eventName: "Famine",
+    description: "Every Player loses half their Resource total.",
+  },
+  {
+    eventName: "Hurricane",
+    description: "Every Player moves half their Movement total (minimum of 1).",
+  },
+  {
+    eventName: "Heavy Fog",
+    description: "Every Player cannot declare any attacks on another Player.",
+  },
+  {
+    eventName: "Earthquakes",
+    description: "Every Player loses bonuses from buildings.",
+  },
+  {
+    eventName: "Year of Bounty",
+    description: "Every Player gains 3 additional Resources.",
+  },
+  {
+    eventName: "Clear Skies",
+    description: "Every Player Movement is increased by 2 for the next round.",
+  },
+  {
+    eventName: "Elemental Tethers",
+    description: "Every Player cannot use any Treasure cards.",
+  },
+  {
+    eventName: "Labor Surplus",
+    description: "All building costs are reduced by 1.",
+  },
+  {
+    eventName: "Labor Shortage",
+    description: "All building costs are increased by 2.",
+  },
+  {
+    eventName: "Treasure Trove",
+    description: "Any Treasure cards received grants 1 additional Treasure card.",
+  },
+  {
+    eventName: "Wild Fires",
+    description: "Every Player must choose 1 building and destory it.",
+  },
+  {
+    eventName: "Healing Waters",
+    description: "Every Player is restored to full health.",
+  },
+  {
+    eventName: "On the Hunt",
+    description: "Every Titan draws 2 Titan Event cards.",
+  },
+  {
+    eventName: "Plague of Rust",
+    description: "Every Player must discard 1 Equipment card.",
+  },
+  {
+    eventName: "Beast Migration",
+    description: "All Monsters receive additional +1 to all stats.",
+  },
+  {
+    eventName: "Drought",
+    description: "All Tile bonuses provided to Buildings are negated.",
+  },
+];
