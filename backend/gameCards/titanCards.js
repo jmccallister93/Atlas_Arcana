9a4622aed7ff15dbc1272dba7dca4aadebd56085
jmccallister93 <jaredmccallister93@gmail.com>
2023-11-28 11:17:19 -0800

    Added titan cards.

---
+++ backend/gameCards/titanCards.js
@@ -0,0 +1,34 @@
+const titanCards = [
+  {
+    titanName: "Fire Titan",
+    rank: 1,
+    health: 5,
+    offense: 2,
+    defense: 0,
+    stamina: 1,
+  },
+  {
+    titanName: "Stone Titan",
+    rank: 1,
+    health: 5,
+    offense: 0,
+    defense: 2,
+    stamina: 1,
+  },
+  {
+    titanName: "Ice Titan",
+    rank: 1,
+    health: 7,
+    offense: 0,
+    defense: 0,
+    stamina: 1,
+  },
+  {
+    titanName: "Storm Titan",
+    rank: 1,
+    health: 5,
+    offense: 0,
+    defense: 0,
+    stamina: 2,
+  },
+];
