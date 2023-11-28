9b2487c1a040f81629faf90541de9170acafae59
jmccallister93 <jaredmccallister93@gmail.com>
2023-11-28 11:03:37 -0800

    Added quest cards

---
+++ backend/gameCards/questCards.js
@@ -0,0 +1,102 @@
+const questCards = [
+  {
+    questName: "Desert Trek",
+    questType: "Movement",
+    description: "Cross 4 Desert Tiles in a single turn.",
+  },
+  {
+    questName: "Frozen Trail",
+    questType: "Movement",
+    description: "Cross 4 Tundra Tiles in a single turn.",
+  },
+  {
+    questName: "Wanderer",
+    questType: "Movement",
+    description: "Cross 4 Grassland Tiles in a single turn.",
+  },
+  {
+    questName: "Forrester",
+    questType: "Movement",
+    description: "Cross 4 Forest Tiles in a single turn.",
+  },
+  {
+    questName: "Promise Land",
+    questType: "Movement",
+    description: "Cross 3 Oasis Tiles in a single turn.",
+  },
+  {
+    questName: "Aggressive Expansion",
+    questType: "Building",
+    description: "Build 1 building next to another Player’s building.",
+  },
+  {
+    questName: "Diversify",
+    questType: "Building",
+    description: "Build 2 buildings on every Tile type.",
+  },
+  {
+    questName: "Reclamation Project",
+    questType: "Building",
+    description: "After a building has been destroyed, build the same building on that same Tile.",
+  },
+  {
+    questName: "Close Quarters",
+    questType: "Building",
+    description: "Build a building next to a Titan.",
+  },
+  {
+    questName: "Questionable Architecture",
+    questType: "Building",
+    description: "Build 5 buildings in a straight line.",
+  },
+  {
+    questName: "Survivalist",
+    questType: "Combat",
+    description: "Win a combat while at less than half your maximum health.",
+  },
+  {
+    questName: "Tactical Genius",
+    questType: "Combat",
+    description: "Win a Player vs Player combat without taking any damage.",
+  },
+  {
+    questName: "Duelist's Pride",
+    questType: "Combat",
+    description: "Challenge and defeat a player in combat who has more Victory Points than you.",
+  },
+  {
+    questName: "Conqueror's March",
+    questType: "Combat",
+    description: "Win 3 combats in 3 consecutive turns.",
+  },
+  {
+    questName: "Unyielding Force",
+    questType: "Combat",
+    description: "Successfully defend against 2 different Players' and/or Titans' attacks in consecutive turns.",
+  },
+  {
+    questName: "Greedy Explorer",
+    questType: "Treasure",
+    description: "Collect 3 Treasure Hoards.",
+  },
+  {
+    questName: "Big Spender",
+    questType: "Treasure",
+    description: "Collect and use 10 Treasure cards.",
+  },
+  {
+    questName: "Collector's Dream",
+    questType: "Treasure",
+    description: "Collect and use 3 different Potion Treasures.",
+  },
+  {
+    questName: "Hoarder's Trove",
+    questType: "Treasure",
+    description: "Collect 7 Treasure cards in your hand at once.",
+  },
+  {
+    questName: "Best Serverd Cold",
+    questType: "Treasure",
+    description: "Use 3 Treasure cards to negatively effect any Player.",
+  },
+];
