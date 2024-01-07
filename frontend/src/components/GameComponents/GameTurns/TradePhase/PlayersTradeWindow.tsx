80c506e95fb9d50cb7a69a20942e0d8db2192655
jmccallister93 <jaredmccallister93@gmail.com>
2024-01-07 10:08:46 -0800

    Trying to add to offers

--- frontend/src/components/GameComponents/GameTurns/TradePhase/PlayersTradeWindow.tsx
+++ frontend/src/components/GameComponents/GameTurns/TradePhase/PlayersTradeWindow.tsx
@@ -2,48 +2,229 @@ import { useState } from "react";
 import { EquipmentItem, PlayerInfo, TreasureItem } from "../../Interfaces";
 import FromPlayerTrade from "./FromPlayerTrade";
 import ToPlayerTrade from "./ToPlayerTrade";
-import { IonModal } from "@ionic/react";
-import "../GameTurn.scss"
+import { IonButton, IonCheckbox, IonItem, IonModal } from "@ionic/react";
+import "../GameTurn.scss";
+import { useAuth } from "../../../../context/AuthContext/AuthContext";
+import { useGameContext } from "../../../../context/GameContext/GameContext";
+import { TypeGuard } from "@reduxjs/toolkit/dist/tsHelpers";
 
 interface PlayersTradewindowProps {
   tradePartnerId: PlayerInfo | undefined;
 }
-const PlayersTradeWindow: React.FC<PlayersTradewindowProps> = ({tradePartnerId}) => {
-  const [tradeState, setTradeState] = useState({
-    fromPlayerOffer: {
+const PlayersTradeWindow: React.FC<PlayersTradewindowProps> = ({
+  tradePartnerId,
+}) => {
+  const { gameState } = useGameContext();
+  const auth = useAuth();
+  const currentPlayer = gameState.players.find(
+    (player) => player.username === auth.username
+  );
+
+  interface TradeOffer {
+    equipment: EquipmentItem[];
+    treasures: TreasureItem[];
+    resources: number;
+  }
+
+  type ItemType = "equipment" | "treasures";
+
+  interface TradeState {
+    firstPlayerOffer: TradeOffer;
+    secondPlayerOffer: TradeOffer;
+  }
+
+  const [tradeState, setTradeState] = useState<TradeState>({
+    firstPlayerOffer: {
       equipment: [],
       treasures: [],
       resources: 0,
     },
-    toPlayerOffer: {
+    secondPlayerOffer: {
       equipment: [],
       treasures: [],
       resources: 0,
     },
   });
 
-  console.log("tradePartnerId", tradePartnerId);
+  const [resourcesToTrade, setResourcesToTrade] = useState<number>(0);
+  const incrementResources = () => {
+    if (resourcesToTrade >= currentPlayer?.inventory.resources) {
+      return;
+    } else {
+      setResourcesToTrade(resourcesToTrade + 1);
+    }
+  };
+
+  const decrementResources = () => {
+    if (resourcesToTrade > 0) {
+      setResourcesToTrade(resourcesToTrade - 1);
+    }
+  };
+
+  const addToOffer = (
+    player: string | undefined,
+    item: EquipmentItem | TreasureItem,
+    itemType: ItemType
+  ) => {
+    const offerKey =
+      player === currentPlayer?.username
+        ? "firstPlayerOffer"
+        : "secondPlayerOffer";
+
+    setTradeState((prevState) => {
+      // Clone the previous state's offer for the specific player
+      const updatedOffer = { ...prevState[offerKey] };
+
+      if (itemType === "equipment" && isEquipmentItem(item)) {
+        updatedOffer.equipment = [...updatedOffer.equipment, item];
+      } else if (itemType === "treasures" && isTreasureItem(item)) {
+        updatedOffer.treasures = [...updatedOffer.treasures, item];
+      }
+
+      return {
+        ...prevState,
+        [offerKey]: updatedOffer,
+      };
+    });
+  };
+
+  // Type guard for EquipmentItem
+  function isEquipmentItem(
+    item: EquipmentItem | TreasureItem
+  ): item is EquipmentItem {
+    return (item as EquipmentItem).equipmentName !== undefined;
+  }
+
+  // Type guard for TreasureItem
+  function isTreasureItem(
+    item: EquipmentItem | TreasureItem
+  ): item is TreasureItem {
+    return (item as TreasureItem).treasureName !== undefined;
+  }
+
+  const removeFromOffer = (
+    player: string | undefined,
+    item: EquipmentItem | TreasureItem,
+    itemType: ItemType
+  ) => {
+    const offerKey =
+      player === currentPlayer?.username
+        ? "firstPlayerOffer"
+        : "secondPlayerOffer";
+    setTradeState((prevState) => ({
+      ...prevState,
+      [offerKey]: {
+        ...prevState[offerKey],
+        [itemType]: prevState[offerKey][itemType].filter((i) => i !== item),
+      },
+    }));
+  };
+
+  const isItemInCurrentPlayerOffer = (
+    item: EquipmentItem | TreasureItem,
+    itemType: ItemType
+  ): boolean => {
+    const offer =
+      currentPlayer?.username === auth.username
+        ? tradeState.firstPlayerOffer
+        : tradeState.secondPlayerOffer;
+
+    if (itemType === "equipment" && isEquipmentItem(item)) {
+      return offer.equipment.includes(item);
+    } else if (itemType === "treasures" && isTreasureItem(item)) {
+      return offer.treasures.includes(item);
+    }
+
+    return false;
+  };
+
+  const toggleItemInCurrentPlayerOffer = (
+    item: EquipmentItem | TreasureItem,
+    itemType: ItemType
+  ) => {
+    if (isItemInCurrentPlayerOffer(item, itemType)) {
+      removeFromOffer(currentPlayer?.username, item, itemType);
+    } else {
+      addToOffer(currentPlayer?.username, item, itemType);
+    }
+  };
 
   return (
     <div className="tradeWindowContainer">
-      Trade Window
-      <div>{tradePartnerId?.username}</div>
-      {/* <FromPlayerTrade
-        showFromPlayerTrade={showFromPlayerTrade}
-        setShowFromPlayerTrade={setShowFromPlayerTrade}
-        tradeOffer={tradeState.fromPlayerOffer}
-        setTradeOffer={(offer) =>
-          setTradeState((prev) => ({ ...prev, fromPlayerOffer: offer }))
-        }
-      />
-      <ToPlayerTrade
-        showToPlayerTrade={showToPlayerTrade}
-        setShowToPlayerTrade={setShowToPlayerTrade}
-        tradeOffer={tradeState.toPlayerOffer}
-        setTradeOffer={(offer) =>
-          setTradeState((prev) => ({ ...prev, toPlayerOffer: offer }))
-        }
-      /> */}
+      <div className="tradeWindowHeader">
+        Trading with {tradePartnerId?.username}
+      </div>
+      <div className="tradeWindowDetailsContainer">
+        <div className="currentPlayerInventory">
+          <h3>Select Equipment to Trade:</h3>
+          {currentPlayer?.inventory.equipment.map((equipmentItem) => (
+            <IonItem key={equipmentItem.equipmentName}>
+              {equipmentItem.equipmentName}{" "}
+              <IonCheckbox
+                checked={isItemInCurrentPlayerOffer(equipmentItem, "equipment")}
+                onIonChange={() =>
+                  toggleItemInCurrentPlayerOffer(equipmentItem, "equipment")
+                }
+              ></IonCheckbox>
+            </IonItem>
+          ))}
+          <h3>Select Treasures to Trade:</h3>
+          {currentPlayer?.inventory.treasures.map((treasureItem) => (
+            <IonItem key={treasureItem.treasureName}>
+              {treasureItem.treasureName}
+              <IonCheckbox
+                checked={isItemInCurrentPlayerOffer(treasureItem, "treasures")}
+                onIonChange={() =>
+                  toggleItemInCurrentPlayerOffer(treasureItem, "treasures")
+                }
+              ></IonCheckbox>
+            </IonItem>
+          ))}
+          <h3>Select Resources to Trade:</h3>
+          <IonItem>
+            <IonButton onClick={decrementResources}>-</IonButton>
+            <span>{currentPlayer?.inventory.resources}</span>
+            <IonButton onClick={incrementResources}>+</IonButton>
+            <IonCheckbox></IonCheckbox>
+          </IonItem>
+        </div>
+        <div className="partnerTradeItems">
+          <h3>Trade offer from {tradePartnerId?.username}</h3>
+          <div>
+            <h4>Equipment:</h4>
+
+            <IonItem>
+              {tradeState[
+                currentPlayer?.username === auth.username
+                  ? "secondPlayerOffer"
+                  : "firstPlayerOffer"
+              ].equipment.map((item) => (
+                <li key={item.equipmentName}>{item.equipmentName}</li>
+              ))}
+            </IonItem>
+            <h4>Treasures:</h4>
+            <IonItem>
+              {tradeState[
+                currentPlayer?.username === auth.username
+                  ? "secondPlayerOffer"
+                  : "firstPlayerOffer"
+              ].treasures.map((item) => (
+                <li key={item.treasureName}>{item.treasureName}</li>
+              ))}
+            </IonItem>
+            <h4>Resources:</h4>
+            <p>
+              {
+                tradeState[
+                  currentPlayer?.username === auth.username
+                    ? "secondPlayerOffer"
+                    : "firstPlayerOffer"
+                ].resources
+              }
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
