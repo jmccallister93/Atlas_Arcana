13b2cec9c4afd57c83f5f27e150c48dab3bd45ec
jmccallister93 <jaredmccallister93@gmail.com>
2024-01-12 09:46:17 -0800

    Treasures and resrouces added to trade on server.

--- backend/matchmaking/gameSessionManager.js
+++ backend/matchmaking/gameSessionManager.js
@@ -395,22 +395,26 @@ const finalizeTrade = async (tradeSessionId, sessionId) => {
 
     try {
       // Swap equipment
-      console.log("Player1Inv preprocess:",player1.inventory)
+      console.log("Player1Inv preprocess:", player1.inventory);
       processTradeItems(
         player1.inventory,
         player2.inventory,
         player1Trade.equipment,
-        player2Trade.equipment
+        player2Trade.equipment,
+        player1Trade.treasures,
+        player2Trade.treasures,
+        player1Trade.resources,
+        player2Trade.resources
       );
-      console.log("Player1Inv postprocessing: ",player1.inventory)
-      console.log("SessionData: ", sessionData.players)
+      console.log("Player1Inv postprocessing: ", player1.inventory);
+      console.log("SessionData: ", sessionData.players);
       // Update the game state in the database
       await sessionClient.set(sessionId, JSON.stringify(sessionData));
 
       // Reset the trade session
       await sessionClient.set(tradeSessionId, JSON.stringify({}));
 
-      return sessionData
+      return sessionData;
       // Commit transaction here if your database supports it
     } catch (error) {
       // Rollback transaction here if your database supports it
@@ -424,18 +428,54 @@ const finalizeTrade = async (tradeSessionId, sessionId) => {
 function processTradeItems(
   player1Inventory,
   player2Inventory,
-  player1TradeItems,
-  player2TradeItems
+  player1EquipmentItems,
+  player2EquipmentItems,
+  player1TreasureItems,
+  player2TreasureItems,
+  player1Resources,
+  player2Resources
 ) {
   // Assuming player1TradeItems and player2TradeItems are arrays of equipment objects
   player1Inventory.equipment = player1Inventory.equipment
-    .filter((item) => !player1TradeItems.some((tradeItem) => tradeItem.equipmentName === item.equipmentName))
-    .concat(player2TradeItems);
+    .filter(
+      (item) =>
+        !player1EquipmentItems.some(
+          (tradeItem) => tradeItem.equipmentName === item.equipmentName
+        )
+    )
+    .concat(player2EquipmentItems);
 
   player2Inventory.equipment = player2Inventory.equipment
-    .filter((item) => !player2TradeItems.some((tradeItem) => tradeItem.equipmentName === item.equipmentName))
-    .concat(player1TradeItems);
- 
+    .filter(
+      (item) =>
+        !player2EquipmentItems.some(
+          (tradeItem) => tradeItem.equipmentName === item.equipmentName
+        )
+    )
+    .concat(player1EquipmentItems);
+
+  player1Inventory.treasures = player1Inventory.treasures
+    .filter(
+      (item) =>
+        !player1TreasureItems.some(
+          (tradeItem) => tradeItem.treasureName === item.treasureName
+        )
+    )
+    .concat(player2TreasureItems);
+
+  player2Inventory.treasures = player2Inventory.treasures
+    .filter(
+      (item) =>
+        !player2TreasureItems.some(
+          (tradeItem) => tradeItem.treasureName === item.treasureName
+        )
+    )
+    .concat(player1Inventory);
+  // Update resources for both players
+  player1Inventory.resources =
+    player1Inventory.resources - player1Resources + player2Resources;
+  player2Inventory.resources =
+    player2Inventory.resources - player2Resources + player1Resources;
 }
 
 // Function to handle player disconnection
